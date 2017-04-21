d58ab5aaa4e68dbdda678d64494c7097d9f2a08c
ShiranuiNui <b1017120@fun.ac.jp>
2017-04-21 17:57:58 +0900

    [add]Interfaceを追加
    TwitterとMastodonの共通する機能をInterfaceで定義してみた。
    でもInterfaceってこういう使い方で合ってるんですかね（困惑）

---
+++ ShiranuiStream/IMiniblog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShiranuiStream
+{
+    /// <summary>
+    /// TwitterとMastodonの共通する機能をまとめたインターフェースです
+    /// </summary>
+    interface IMiniblog
+    {
+        /// <summary>
+        /// Twitter:Tweet/Mastodon:Toot
+        /// </summary>
+        void Post();
+        /// <summary>
+        /// Twitter:ReTweet/Mastodon:Boost
+        /// </summary>
+        void Reblog();
+        /// <summary>
+        /// Twitter:Reply/Mastodon:Reply
+        /// </summary>
+        void Reply();
+        /// <summary>
+        /// Twitter:Favorite/Mastodon:Favourite
+        /// </summary>
+        void Favourite();
+    }
+}

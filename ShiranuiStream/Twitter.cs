055121b61dcce54b3413bb3ad2c32b2b39c9d05c
ShiranuiNui <b1017120@fun.ac.jp>
2017-04-24 11:06:11 +0900

    [change]コードスタイルの改定
    1行のみのメソッドをラムダ式のみに優先するよう設定

--- ShiranuiStream/Twitter.cs
+++ ShiranuiStream/Twitter.cs
@@ -11,30 +11,26 @@ namespace ShiranuiStream
     class Twitter : IMiniblog
     {
         private Tokens Tokens { get; }
-        public Twitter()
-        {
+
+        public Twitter() =>
             this.Tokens = Tokens.Create(
                 "{API key}",
                 "{API secret}",
                 "{Access token}",
                 "{Access token secret}");
-        }
+
         public async Task PostStatus(string text)
-        {
-            await this.Tokens.Statuses.UpdateAsync(status => text);
-        }
+         => await this.Tokens.Statuses.UpdateAsync(status => text);
+
         public async Task Reply(string text, long tweetid)
-        {
-            await this.Tokens.Statuses.UpdateAsync(status => text, in_reply_to_status_id => tweetid);
-        }
+         => await this.Tokens.Statuses.UpdateAsync(status => text, in_reply_to_status_id => tweetid);
+
         public async Task Reblog(long tweetid)
-        {
-            await this.Tokens.Statuses.RetweetAsync(id => tweetid);
-        }
+         => await this.Tokens.Statuses.RetweetAsync(id => tweetid);
+
         public async Task Favourite(long tweetid)
-        {
-            await this.Tokens.Favorites.CreateAsync(id => tweetid);
-        }
+         => await this.Tokens.Favorites.CreateAsync(id => tweetid);
+
         public /*async*/ void StartStreaming()//実装中。以下のコードはデバッグ用
         {
             var disposable = this.Tokens.Streaming.UserAsObservable().OfType<StatusMessage>().Subscribe
